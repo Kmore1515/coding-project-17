b9cb8372e460da3f443397bb923f52ead6a51e70
Kmore1515 <kylebradleymorris@usf.edu>
2024-11-24 22:27:42 -0500

    Added missing parts of last commit

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -2,12 +2,29 @@ import { useState, useEffect } from 'react'
 
 function TourDashbouard() {
     const [tours, setTours] = useState([]);
+    const [Loading, setLoading] = useState(false)
+    const [showMore, setShowMore] = useState({})
+    const [error, setError] = useState(null)
 
     useEffect(() => {
         fetch(`https://www.course-api.com/react-tours-project`) // fetching from the api
             .then(response => response.json())
             .then(data => setTours(data));
     }, []);
+
+    const handleNotInterested = (id) => {
+        setTours(tours.filter((tour) => tour.id !== id))
+    };
+
+    const handleShowMore = (id) => {
+        setShowMore(prevState => ({...prevState, [id]: !prevState[id]}))
+    };
+        if (Loading) {
+            return <h3> LOADING...</h3>
+        }
+        if (error) {
+            return <h3> Error, please try again.</h3>
+        }
     return (
         <>
         <h2>Tours</h2>
@@ -15,9 +32,11 @@ function TourDashbouard() {
             {tours.map(tour => ( // Grabing all the names, info, images, and prices to display them.
                 <li key={tour.id}>
                     <h3> <u>Tour Name: {tour.name}</u></h3> 
-                    <p> <b>Information:</b> {tour.info}</p> 
                     <img src={tour.image}/> 
                     <p>Price: ${tour.price}</p>
+                    <p>{showMore[tour.id] ? tour.info : `${tour.info.substring(0,150)}...`}</p>
+                    <button onClick={() => handleNotInterested(tour.id)}> Not Interested </button>
+                    <button onClick={() => handleShowMore(tour.id)}> Show More </button>
                 </li>
             ))}
 
